2883b9fef6e66ef87cd38b7d90a00a095ceef9bd
Daniel Grace <dgrace@gracefulcode.com>
2019-06-25 10:24:07 -0500

    Forgot this file

---
+++ src/main/java/com/gracefulcode/opengine/renderers/vulkan/VulkanPlatform.java
@@ -0,0 +1,7 @@
+package com.gracefulcode.opengine.renderers.vulkan;
+
+import com.gracefulcode.opengine.core.Platform;
+
+public interface VulkanPlatform extends Platform<Vulkan> {
+	public void setInstance(org.lwjgl.vulkan.VkInstance instance);
+}
